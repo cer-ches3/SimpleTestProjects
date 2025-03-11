6045b80688375c64847039735d6bfd62bda30ed3
sergey <cer-ches3@yandex.ru>
2025-03-11 16:12:49 +0300

    add support lombok

--- RockPapperScissors/src/main/java/User.java
+++ RockPapperScissors/src/main/java/User.java
@@ -1,21 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
     private String name;
     private int score = 0;
 
     public User(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getScore() {
-        return score;
     }
 
     public int increaseScore() {
