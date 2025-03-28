aa575c05ebf2547621d572a899d4e430611fc83b
sergey <cer-ches3@yandex.ru>
2025-03-28 10:19:07 +0300

    create empty project CaloriesTrackingService

---
+++ CaloriesTrackingService/src/main/java/com/example/CaloriesTrackingService/CaloriesTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CaloriesTrackingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesTrackingServiceApplication.class, args);
+	}
+
+}
---
+++ CaloriesTrackingService/src/test/java/com/example/CaloriesTrackingService/CaloriesTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CaloriesTrackingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesTrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
